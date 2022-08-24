3e9732aec1304ed0d4905ff4c88402ab72bb8603
Jonathan charlet <jonathan.charlet.pro@gmail.com>
2022-08-24 22:05:44 +0200

    Modifications CSS pages

--- admin/src/components/converter/ConverterForm.vue
+++ admin/src/components/converter/ConverterForm.vue
@@ -6,17 +6,17 @@ export default {
 
 <template>
 <div class=" text-center">
-    <div class="  formBackground">
+    <div class="formBackground">
         <form>
             <h1 class=" text-center font-monospace pb-4">MoneyValue</h1>
             <div class=" row m-auto col-lg-12">
 
-                <div class="m-auto col-lg-6">
-                    <input type="email" placeholder="Somme" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div class="m-auto search_box col-lg-6">
+                    <input type="text" placeholder="Somme" class="form-control formBg" id="" aria-describedby="">
                 </div>
 
                 <div class=" col-lg-6">
-                    <select class="form-select" aria-label="Default select example">
+                    <select class="form-select converterSelect" aria-label="Default select example">
                         <option selected>EUR --> USD</option>
                         <option value="1">USD --> EUR</option>
                         <option value="2">Two</option>
@@ -25,11 +25,11 @@ export default {
                 </div>
             </div>
         
-            <div class="mb-3 pt-4 m-auto col-lg-6">
-                <input type="password" class="form-control" id="exampleInputPassword1">
+            <div class="search_box mb-3 pt-4 m-auto col-lg-6">
+                <input type="text" class="form-control" id="">
             </div>
             <div class=" py-2 text-center">
-                <button type="submit" class="btn font-monospace btn-primary col-lg-4">Convertir</button>
+                <button type="submit" class="btn font-monospace btnConverter col-lg-4">Convertir</button>
             </div>
 
         
@@ -50,6 +50,31 @@ export default {
     .br{
         border: 1px solid red;
     }
+
+    .btnConverter{
+        opacity: 70%;
+        background: rgb(2, 116, 215); 
+        color: white;
+   
+    }
+
+    .btnConverter:hover{
+        opacity: 80%;
+        background: none;
+        color : black;
+    }
+
+    .search_box input[type="text"]{
+        
+        backdrop-filter: blur(10%);
+        color: black;
+    }
+
+    .converterSelect{
+        
+        
+        backdrop-filter: blur(10%);
+        }
 
   
 
