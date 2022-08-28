b92aa525e333be203c67d24e57bc85df5b6f2f25
Jonathan charlet <jonathan.charlet.pro@gmail.com>
2022-08-28 12:22:21 +0200

    URL permettant de vérifier si le service est fonctionnel ou non

--- api/routes/api.php
+++ api/routes/api.php
@@ -15,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/ping', function () {
+    return response('The service is functional', 200)->header('Content-Type', 'text/plain');
+});
 Route::resource('currencies', CurrencyController::class);
 Route::resource('pairs', PairsController::class);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
